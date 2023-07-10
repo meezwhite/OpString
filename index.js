8cc24414aab2ba643e426347aa2d2f6235618c48
meezwhite <meezwhite@proton.me>
2023-07-11 00:47:01 +0200

    fix(setSequence): restore previous commit

--- index.js
+++ index.js
@@ -214,7 +214,7 @@ export default class OpString {
      */
     setSequence(sequence) {
         try {
-            this.#validateArguments('set sequence', arguments);
+            this.#validateArguments('setSequence', arguments);
             this.#sequence = sequence;
             this.#sequenceData = [];
             for (let i = 0; i < sequence.length; i++) {
